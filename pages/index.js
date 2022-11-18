d67599adde15ac71e18a9b61023d6a4e8a186bf9
ZProtons <117505835+ZProtons@users.noreply.github.com>
2022-11-18 22:45:28 +0530

    Update

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+
 export default function Home() {
   return (
     //HTML
@@ -10,7 +11,25 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      //mess
+      <video
+    id="my-video"
+    class="video-js"
+    controls
+    preload="auto"
+    width="640"
+    height="264"
+    poster="MY_VIDEO_POSTER.jpg"
+    data-setup="{}"
+  >
+    <source src="videoplayback.mp4" type="video/mp4" />
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a
+      web browser that
+      <a href="https://videojs.com/html5-video-support/" target="_blank"
+        >supports HTML5 video</a
+      >
+    </p>
+  </video>
       <video
     id="my-video"
     class="video-js"
@@ -32,9 +51,35 @@ export default function Home() {
   </video>
 
   <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
+      
+=======
+  <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
       //mess
       <main>
         <Header title="Welcome to my app!" />
+        //EDITED
+        <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
+        <video
+          id="my-video"
+          class="video-js"
+          controls
+          preload="auto"
+          width="640"
+          height="264"
+          poster="MY_VIDEO_POSTER.jpg"
+          data-setup="{}"
+      >
+          <source src="/pages/video.mp4" type="video/mp4" />
+          <source src="pages/video.mp4" type="video/mp4" />
+          <source src="video.mp4" type="video/mp4" />
+          <p class="vjs-no-js">
+            To view this video please enable JavaScript, and consider upgrading to a
+            web browser that
+          <a href="https://videojs.com/html5-video-support/" target="_blank"
+            >supports HTML5 video</a
+          >
+    </p>
+  </video>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
@@ -43,5 +88,6 @@ export default function Home() {
   
       <Footer />
     </div>
+    //HTML
   )
 }
