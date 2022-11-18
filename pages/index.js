8ce8658bb78d8f1cb2f656403edd45d0b73eb725
ZProtons <117505835+ZProtons@users.noreply.github.com>
2022-11-18 22:51:20 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -5,89 +5,9 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    //HTML
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <video
-    id="my-video"
-    class="video-js"
-    controls
-    preload="auto"
-    width="640"
-    height="264"
-    poster="MY_VIDEO_POSTER.jpg"
-    data-setup="{}"
-  >
-    <source src="videoplayback.mp4" type="video/mp4" />
-    <p class="vjs-no-js">
-      To view this video please enable JavaScript, and consider upgrading to a
-      web browser that
-      <a href="https://videojs.com/html5-video-support/" target="_blank"
-        >supports HTML5 video</a
-      >
-    </p>
-  </video>
-      <video
-    id="my-video"
-    class="video-js"
-    controls
-    preload="auto"
-    width="640"
-    height="264"
-    poster="MY_VIDEO_POSTER.jpg"
-    data-setup="{}"
-  >
-    <source src="video.mp4" type="video/mp4" />
-    <p class="vjs-no-js">
-      To view this video please enable JavaScript, and consider upgrading to a
-      web browser that
-      <a href="https://videojs.com/html5-video-support/" target="_blank"
-        >supports HTML5 video</a
-      >
-    </p>
-  </video>
-
-  <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
-      
-=======
-  <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
-      //mess
-      <main>
-        <Header title="Welcome to my app!" />
-        //EDITED
-        <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
-        <video
-          id="my-video"
-          class="video-js"
-          controls
-          preload="auto"
-          width="640"
-          height="264"
-          poster="MY_VIDEO_POSTER.jpg"
-          data-setup="{}"
-      >
-          <source src="/pages/video.mp4" type="video/mp4" />
-          <source src="pages/video.mp4" type="video/mp4" />
-          <source src="video.mp4" type="video/mp4" />
-          <p class="vjs-no-js">
-            To view this video please enable JavaScript, and consider upgrading to a
-            web browser that
-          <a href="https://videojs.com/html5-video-support/" target="_blank"
-            >supports HTML5 video</a
-          >
-    </p>
-  </video>
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-    //HTML
-  
-      <Footer />
+    <div>
+      <h2>NextJs VideoPlayer - GeeksforGeeks</h2>
+      <ReactPlayer url='https://www.youtube.com/watch?v=wWgIAphfn2U' />
     </div>
-    //HTML
   )
 }
