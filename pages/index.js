52b81e1849426d8b848703c364561ff22d8b9365
ZProtons <117505835+ZProtons@users.noreply.github.com>
2022-11-18 22:35:07 +0530

    Bin

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,43 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
+    //HTML
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      //mess
+      <video
+    id="my-video"
+    class="video-js"
+    controls
+    preload="auto"
+    width="640"
+    height="264"
+    poster="MY_VIDEO_POSTER.jpg"
+    data-setup="{}"
+  >
+    <source src="video.mp4" type="video/mp4" />
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a
+      web browser that
+      <a href="https://videojs.com/html5-video-support/" target="_blank"
+        >supports HTML5 video</a
+      >
+    </p>
+  </video>
 
+  <script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
+      //mess
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
+    //HTML
+  
       <Footer />
     </div>
   )
